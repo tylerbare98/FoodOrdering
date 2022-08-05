242919270a13b545184a0bb18f53bf0dbd24b2fe
tylerbare98 <tylerbare98@gmail.com>
2022-08-05 13:14:51 +0000

    .

--- src/components/Meals/MealsItem.jsx
+++ src/components/Meals/MealsItem.jsx
@@ -1,12 +1,39 @@
+import {useState} from 'react';
 import styles from "./MealsItem.module.css"
 
 const MealsItem = (props) => {
+
+    const [amount, setAmount] = useState("1");
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        console.log("form submitted with amount = " + amount)
+      }
+
+    const amountChangedHandler = (event) => {
+        setAmount(event.target.value)
+    }
+
+    let mealPrice=`$${props.price.toFixed(2)}`
     return(
-        <ul>
-            <h3>{props.name}</h3>
-            <p className={styles.description}>{props.description}</p>
-            <p className={styles.price}>{props.price}</p>
-        </ul>
+        <div className={styles.container}>
+            <section>
+                <h2>{props.name}</h2>
+                <p className={styles.description}>{props.description}</p>
+                <p className={styles.price}>{mealPrice}</p>
+            </section>
+            <form onSubmit={submitHandler} className={styles.form}>
+                <label className={styles.amountLabel}>Amount:
+                    <input 
+                    type="number" min="00" max="20" step="1" 
+                    value={amount}
+                    onChange={amountChangedHandler}
+                    className={styles.amountInput}
+                    />
+                </label><br/>
+                <input type="submit" value="+ Add" className={styles.amountSubmit}/>
+            </form>
+        </div>
     )
 }
 
